83057e8ae57f3c42c95735130427f5bf531ddf97
Jtune89 <Joseph.tune1@gmail.com>
2023-01-10 09:52:37 +0000

    json web token authentication

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,10 @@ from .serializers import ProfileSerializer
 
 
 class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
@@ -19,8 +23,8 @@ class ProfileList(generics.ListAPIView):
     ]
     filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
-
     ordering_fields = [
         'posts_count',
         'followers_count',
@@ -31,6 +35,9 @@ class ProfileList(generics.ListAPIView):
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
